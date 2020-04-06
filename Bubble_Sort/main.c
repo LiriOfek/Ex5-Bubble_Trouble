8107bf9594aad1dfc7f0b9fc146d7bedb38d7715
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-06 09:30:44 +0300

    add main file with documentation

---
+++ Bubble_Sort/main.c
@@ -0,0 +1,5 @@
+/********************************************************\
+File Name: main.c
+Author: Liri (6/4/2020)
+Purpose: This file sort given array, with given size
+\********************************************************/
